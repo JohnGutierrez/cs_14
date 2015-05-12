3e09dede43f6935eeee0b5a5a8717e53cf9b70d8
John Gutierrez <jguti039@ucr.edu>
2015-05-11 17:24:40 -0700

    lab5

---
+++ main.cc
@@ -0,0 +1,31 @@
+//John Gutierrez
+//861167274
+
+#include "lab5.h"
+
+#include <iostream>
+#include <cassert>
+#include <map>
+#include <cstdlib>
+#include <list>
+#include <math.h>
+using namespace std;
+#define nil 0
+
+int main()
+{
+    BST<int> test1;
+    test1.insert(20);
+    test1.insert(30);
+    test1.insert(10);
+    test1.insert(12);
+    test1.insert(90);
+    test1.insert(31);
+    test1.insert(1);
+    test1.insert(42);
+    test1.insert(6);
+    test1.insert(13);
+    test1.insert(81);
+    test1.insert(70);
+    return 0;
+}
